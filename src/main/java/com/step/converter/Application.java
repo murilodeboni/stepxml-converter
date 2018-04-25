5123614f48652475476394fb4168cc93684a8653
Igor Cellani <igor@cellani.com.br>
2018-04-25 10:39:06 -0400

    Innitial Commit

---
+++ src/main/java/com/step/converter/Application.java
@@ -0,0 +1,17 @@
+package com.step.converter;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@SpringBootApplication
+public class Application {
+
+    public static void main( String args[] ) {
+
+        new SpringApplicationBuilder( Application.class ).properties( "spring.config.name:application" ).build().run( args );
+
+    }
+
+}
